e955972026de4d8f4446110afdd66640ce7b97c3
Md. Shahinur Rahman <coredeveloper.2013@gmail.com>
2024-10-30 15:45:24 +0600

    desktop completed

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div class="bg-gray-100 flex items-center justify-center min-h-screen">
@@ -14,47 +16,95 @@ export default function Home() {
 
           {/* result */}
           <p class="text-2xl font-semibold">Great</p>
-          <p class="text-sm mt-2 opacity-60 px-8">
+          <p class="text-sm mt-2 opacity-60 px-6">
             You scored higher than 65% of the people who have taken these tests.
           </p>
         </div>
 
         {/* <!-- Right Panel --> */}
-        <div class="flex flex-col justify-between w-1/2 p-10">
+        <div class="flex flex-col justify-between w-1/2 p-8">
+          {/* title */}
+          <div>
+            <h2 class="text-lg font-semibold mb-6">Summary</h2>
+          </div>
+
           {/* <!-- Summary Items --> */}
           <div class="space-y-4">
-            <div class="flex justify-between items-center border-b pb-3">
+            <div class="flex justify-between items-center bg-red-50 p-3 rounded-lg">
               <div class="flex items-center space-x-2 text-red-500 font-medium">
-                <span>⚡</span>
+                <span>
+                  <Image
+                    src="/assets/icon-reaction.svg"
+                    height={20}
+                    width={20}
+                    alt=""
+                    priority
+                  />
+                </span>
                 <span>Reaction</span>
               </div>
-              <div class="font-semibold text-gray-700">80 / 100</div>
+              <div class="font-semibold text-gray-700 text-md">
+                80 <span className="text-gray-400"> / 100</span>
+              </div>
             </div>
-            <div class="flex justify-between items-center border-b pb-3">
-              <div class="flex items-center space-x-2 text-yellow-400 font-medium">
-                <span>🧠</span>
+
+            <div class="flex justify-between items-center bg-yellow-50 p-3 rounded-lg">
+              <div class="flex items-center space-x-2 text-yellow-500 font-medium">
+                <span>
+                  <Image
+                    src="/assets/icon-memory.svg"
+                    height={20}
+                    width={20}
+                    alt=""
+                    priority
+                  />
+                </span>
                 <span>Memory</span>
               </div>
-              <div class="font-semibold text-gray-700">92 / 100</div>
+              <div class="font-semibold text-gray-700 text-md">
+                92 <span className="text-gray-400"> / 100</span>
+              </div>
             </div>
-            <div class="flex justify-between items-center border-b pb-3">
-              <div class="flex items-center space-x-2 text-green-500 font-medium">
-                <span>💬</span>
+
+            <div class="flex justify-between items-center bg-teal-50 p-3 rounded-lg">
+              <div class="flex items-center space-x-2 text-teal-500 font-medium">
+                <span>
+                  <Image
+                    src="/assets/icon-verbal.svg"
+                    height={20}
+                    width={20}
+                    alt=""
+                    priority
+                  />
+                </span>
                 <span>Verbal</span>
               </div>
-              <div class="font-semibold text-gray-700">61 / 100</div>
+              <div class="font-semibold text-gray-700 text-md">
+                61 <span className="text-gray-400"> / 100</span>
+              </div>
             </div>
-            <div class="flex justify-between items-center">
-              <div class="flex items-center space-x-2 text-blue-500 font-medium">
-                <span>👁️</span>
+
+            <div class="flex justify-between items-center bg-indigo-50 p-3 rounded-lg">
+              <div class="flex items-center space-x-2 text-indigo-500 font-medium">
+                <span>
+                  <Image
+                    src="/assets/icon-visual.svg"
+                    height={20}
+                    width={20}
+                    alt=""
+                    priority
+                  />
+                </span>
                 <span>Visual</span>
               </div>
-              <div class="font-semibold text-gray-700">72 / 100</div>
+              <div class="font-semibold text-gray-700 text-md">
+                72 <span className="text-gray-400"> / 100</span>
+              </div>
             </div>
           </div>
 
           {/* <!-- Continue Button --> */}
-          <button class="mt-6 bg-purple-600 text-white py-3 rounded-full font-semibold hover:bg-purple-700 transition duration-300">
+          <button class="mt-6 bg-[#303B59] active:bg-purple-600 text-white py-3 rounded-full font-semibold hover:bg-purple-700 transition duration-300">
             Continue
           </button>
         </div>
