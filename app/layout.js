d1cfcea68c6ddddb0b1f4fcc3bde56e64b8cfaf1
Md. Shahinur Rahman <coredeveloper.2013@gmail.com>
2024-10-30 15:11:17 +0600

    snapshot 1

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,9 @@
-import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+import { Hanken_Grotesk } from "next/font/google";
+
+const hankenGrotesk = Hanken_Grotesk({
+  subsets: ["latin"],
 });
 
 export const metadata = {
@@ -20,9 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${hankenGrotesk.className} antialiased`}>
         {children}
       </body>
     </html>
